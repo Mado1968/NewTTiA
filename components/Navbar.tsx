e01cbc2230acb6e9f7d7f7e7424b56c4fbcdd3a9
unknown <cescbetres@gmail.com>
2026-01-10 11:45:05 +0100

    news2026

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar: React.FC = () => {
       <div className="container mx-auto px-6 flex justify-between items-center">
         {/* Logo - Pointer events auto to allow clicking */}
         <a href="#" className="flex items-center gap-2 ">
-          <div className="relative w-8 h-8 flex items-center justify-center bg-ttia-white rounded-full text-white">
+          <div className="relative w-14 h-14 flex items-center justify-center bg-ttia-white rounded-full text-white">
             {<img src="/assets/ttia_New_2026.jpg" alt="Logo"/>/* Logo */}
           </div>
           <span className="text-lg font-bold font-mono tracking-tighter text-ttia-white color-white ">
